bb64950a278529af0e39ffa6aec8367880af95c6
Mr_Faton <firefly90@inbox.ru>
2015-10-29 12:10:07 +0200

    update from home

---
+++ src/main/java/com/mr_faton/core/Test.java
@@ -0,0 +1,18 @@
+package com.mr_faton.core;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Description
+ *
+ * @author Mr_Faton
+ * @since 29.10.2015
+ */
+public class Test {
+    static Logger logger = Logger.getLogger("" +
+            "com.mr_faton.core.Test");
+
+    public static void main(String[] args) {
+        logger.debug("to ot to");
+    }
+}
